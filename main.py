686e09d822118803ea787a46da3f635740e28131
minsiksippi <minsik6755@naver.com>
2022-04-07 22:57:56 +0900

    #1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello World')
+def fun(num):
+    li=list(str(num))
+    li.reverse()
+    n = int(str(''.join(li)))
+    return n
+
+a, b = map(int, input().split())
+print(fun(fun(a)+fun(b)))
